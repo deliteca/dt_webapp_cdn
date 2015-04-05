197d229ff87f2b91491e705f866177c1e15cc2d6
Aaron Diep <ahkdiep@gmail.com>
2015-04-05 16:12:20 -0600

    initial angular setup

---
+++ angular/app/scripts/tsc-out.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=tsc-out.js.map
--- setup.sh
+++ setup.sh
@@ -16,8 +16,14 @@ pip install -r $pip_reqs
 if [ -z $NODE_PATH ]; then
     ln -s /opt/local/bin/python2.7 $VIRTUAL_ENV/bin/python2
     nodeenv -p
-    npm install -g generator-angular --save
     npm install -g grunt-cli --save
+    npm install -g grunt-init --save
+    npm install -g bower --save
+    npm install -g grunt --save
+    npm install -g typescript --save
+    npm install -g generator-angular --save
+    npm install grunt --save-dev
+    mkdir angular
 fi
 
 deactivate
