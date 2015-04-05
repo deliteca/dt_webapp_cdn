e3aade750ea50e2cda41bb2669b171bd82f718eb
Aaron Diep <ahkdiep@gmail.com>
2015-04-05 14:18:34 -0600

    first commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+PROJECT_NAME=dt_webapp_frontend
+pip_reqs=test-requirements.txt
+
+source $DT_PROFILE_LOC/scripts/dt/dt_venv.sh
+setup_venv 3.4
+
+if ! [ "$VIRTUAL_ENV" = "$WORKON_HOME/$PROJECT_NAME" ]; then
+    mkvirtualenv $PROJECT_NAME
+fi
+
+workon $PROJECT_NAME
+
+pip install -r $pip_reqs
+
+if [ -z $NODE_PATH ]; then
+    ln -s /opt/local/bin/python2.7 $VIRTUAL_ENV/bin/python2
+    nodeenv -p
+    npm install -g generator-angular --save
+    npm install -g grunt-cli --save
+fi
+
+deactivate
