a99222464da2f2eab3b68c16358a24605a8c9514
Aaron Diep <ahkdiep@gmail.com>
2015-04-11 23:06:00 -0600

    testing directive

--- app/src/app/components/nodes/nodes.spec.js
+++ app/src/app/components/nodes/nodes.spec.js
@@ -6,30 +6,45 @@ describe('Nodes', function() {
   var $compile;
   var $rootScope;
   var controller;
+  var $httpBackend;
   // create mock module
   beforeEach(module('app'));
 
   // inject mocks so lookup the corresponding mock API docs
-  beforeEach(inject(function($controller, _$rootScope_, _$compile_, $httpBackend) {
-    $httpBackend.whenGET('app/components/nodes/nodes.html').respond(200, '<div>ctrl.name()</div>');
+  beforeEach(inject(function($controller, _$rootScope_, _$compile_, _$httpBackend_) {
     $rootScope = _$rootScope_;
     scope = $rootScope.$new();
     $compile = _$compile_;
+    $httpBackend = _$httpBackend_;
     controller = $controller(dt.webapp.controllerName('Nodes'),
                              {/* no locals */},
                              { uid : 'foo'} /* bindings */
                             );
   }));
 
-  it('should have a defined isolateScope', function() {
-    var html = angular.element('<dt-wa-nodes uid="foo"></dt-wa-nodes>');
-    var elm = $compile(html)(scope);
-    scope.$digest();
-//    expect(elm.isolateScope()).not.toBeDefined();
-    expect(elm.attr('uid')).toEqual('foo');
-  });
-
   it('should get name', function() {
     expect(controller.name()).toEqual('/');
   });
+
+  describe('testing directive', function() {
+    var elm; 
+    beforeEach(function() {
+      var template = '<div>{{ctrl.name()}}</div>';
+      $httpBackend.expectGET('app/components/nodes/nodes.html').respond(template);
+      var html = angular.element('<dt-wa-nodes uid="foo"></dt-wa-nodes>');
+      elm = $compile(html)(scope);
+      $httpBackend.flush();
+      scope.$digest();
+    });
+
+    it('should compiled correctly', function() {
+      expect(elm.attr('uid')).toEqual('foo');
+    });
+
+    it('should have the correct controller binding', function() {
+      expect(elm.html()).toContain('<div class="ng-binding">/</div>');
+    });
+
+  });
+
 });
